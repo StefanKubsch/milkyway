e1deedd0f9f8462c87c59b44fa7921a78419d547
StefanKubsch <stefan.kubsch@outlook.de>
2023-03-05 18:55:44 +0100

    Updated song

--- Src/4klang/4klang.h
+++ Src/4klang/4klang.h
@@ -2,13 +2,13 @@
 #define SAMPLE_RATE 44100
 #define BPM 120.000000
 #define MAX_INSTRUMENTS 5
-#define MAX_PATTERNS 50
+#define MAX_PATTERNS 53
 #define PATTERN_SIZE_SHIFT 4
 #define PATTERN_SIZE (1 << PATTERN_SIZE_SHIFT)
 #define MAX_TICKS (MAX_PATTERNS*PATTERN_SIZE)
 #define SAMPLES_PER_TICK 5512
 #define MAX_SAMPLES (SAMPLES_PER_TICK*MAX_TICKS)
-#define POLYPHONY 2
+#define POLYPHONY 1
 #define FLOAT_32BIT
 #define SAMPLE_TYPE float
 
@@ -16,3 +16,7 @@
 
 // declaration of the external synth render function, you'll always need that
 extern "C" void  __stdcall	_4klang_render(void*);
+// declaration of the external envelope buffer. access only if you're song was exported with that option
+extern "C" float _4klang_envelope_buffer;
+// declaration of the external note buffer. access only if you're song was exported with that option
+extern "C" int   _4klang_note_buffer;
